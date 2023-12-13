e13b5f744fa729335ccc59d4b5fa7ca6fd8fe94b
Imgyu Kim <kimimgo@gmail.com>
2023-12-13 21:20:15 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 231213/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 231213/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -14,10 +14,9 @@ dirs = {
     3: [-1, 0]
 }
 
-def in_range(x, y, S):
+def in_range(x, y):
     return (
-        x >= 0 and x < m and y >= 0 and y < n
-        and (S[x][y] == 0)
+        x >= 0 and x < n and y >= 0 and y < m
     )
 
 n_dirs = 0
@@ -25,23 +24,19 @@ N = 1
 S[0][0] = 1 
 x, y = 0, 0
 while N < m*n:
-    N += 1
     nx, ny = x + dirs[n_dirs][0], y + dirs[n_dirs][1]
-    if in_range(nx, ny, S):
+    if in_range(nx, ny):
+        if S[nx][ny] != 0:
+            n_dirs = (n_dirs + 1) % 4
+        N += 1
         x, y= x + dirs[n_dirs][0], y + dirs[n_dirs][1]
         S[x][y] = N
-        # print(x, y, S)
+        # print(x, y, n_dirs,S)
     else:
         n_dirs = (n_dirs + 1) % 4
-        x, y = x + dirs[n_dirs][0], y + dirs[n_dirs][1]
-        S[x][y] = N
-        # print(x, y, S)
-
-# for x in range(m):  # 열
-#     for y in range(n):  # 행
 
 
-for x in range(m):
+for x in range(n):
     print(
         " ".join(list(map(str,S[x])))
         # S[x]
